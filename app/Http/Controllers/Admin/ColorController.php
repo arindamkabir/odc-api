569214433e935b14efbd948c83798d35c8d48ed6
Arindam Kabir <arindamkabir@gmail.com>
2023-11-30 02:21:08 +0600

    feat: add index function to color controller

--- app/Http/Controllers/Admin/ColorController.php
+++ app/Http/Controllers/Admin/ColorController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Color;
 use Illuminate\Http\Request;
 
 class ColorController extends Controller
 {
+    public function index()
+    {
+        $colors = Color::query()
+            ->paginate(10);
+
+        return response()->json($colors);
+    }
 }
