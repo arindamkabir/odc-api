4face5e12da26bca90fb2c8f13dc8de72ac706be
Arindam Kabir <arindamkabir@gmail.com>
2023-11-30 02:41:29 +0600

    feat: update category form requests

--- app/Http/Requests/Category/StoreCategoryRequest.php
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Category;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCategoryRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,10 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'parent_id' => ['required', 'exists:App\Models\Category,id'],
+            'is_featured' => ['required', Rule::in(['true', 'false'])],
+            'is_hidden' => ['required', Rule::in(['true', 'false'])],
         ];
     }
 }
