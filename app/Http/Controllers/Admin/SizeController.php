95f25e507e88ff3288546ab7e8960f8f1fbd3408
Arindam Kabir <arindamkabir@gmail.com>
2023-11-30 02:21:33 +0600

    feat: add index function to size controller

--- app/Http/Controllers/Admin/SizeController.php
+++ app/Http/Controllers/Admin/SizeController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Size;
 use Illuminate\Http\Request;
 
 class SizeController extends Controller
 {
+
+    public function index()
+    {
+        $sizes = Size::query()
+            ->paginate(10);
+
+        return response()->json($sizes);
+    }
+
     public function store()
     {
     }
