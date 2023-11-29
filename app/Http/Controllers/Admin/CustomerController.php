68319ba66f83e7ec4e46dcd32076423514c3cf83
Arindam Kabir <arindamkabir@gmail.com>
2023-11-30 02:21:23 +0600

    feat: add index function to customer controller

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     public function index()
     {
+        $customers = Customer::query()
+            ->paginate(10);
+
+        return response()->json($customers);
     }
 
     public function show()
